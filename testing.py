fdb396d15165d80e8388e4482e7b0391e079134b
coderarg <lucasgarcia.developer@gmail.com>
2025-11-24 13:49:52 -0300

    Ordernar archivos testing

--- testing.py
+++
@@ -1,9 +0,0 @@
-## Script que ejecuta las funciones y devuelve OK o el error correspondiente
-import compras, finanzas, persistencia, productos, stock, ventas
-
-async def testear_modulos():
-    '''
-    Función que prueba los módulos del sistema según lo esperado por cada uno. 
-    Parámetros
-    - To be defined
-    '''
